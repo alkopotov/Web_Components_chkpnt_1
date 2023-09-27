5954155dc35c776ffc06918e07e6eb9ad789e3a3
AlHobb <118611357+alkopotov@users.noreply.github.com>
2023-09-27 13:22:58 +0300

    Ver 1.0.0

--- data.js
+++ data.js
@@ -1,8 +1,7 @@
 const USERDATA = [
   {
     id: 1,
-    link: '/images/alkopotov.jpeg',
-    name: 'Копотов Алексей Владимирович',
+    name: 'Копотов Алексей',
     company: 'Издательство "За рулем"',
     position: 'Заместитель коммерческого директора',
     birthDate: '25.04.1974',
@@ -13,14 +12,57 @@ const USERDATA = [
   },
   {
     id: 2,
-    link: '/images/pic2.jpeg',
-    name: 'Бессмертный Кащей Батькович',
-    company: 'Тридевятое царство',
-    position: 'Верховный правитель',
-    birthDate: 'три тысячи лет назад',
-    education: 'читает со словарем',
-    skills: 'смертоубийства, пытки',
-    phone: '+6 666 666 66 66',
-    email: 'immortal@gmail.com'
+    name: 'Belson Gavin',
+    company: 'Hooli',
+    position: 'Chief Innovation Officer',
+    birthDate: '29.02.1964',
+    education: 'незаконченное высшее',
+    skills: 'инновации, таргетированный поиск, кросс-платформенные вертикали',
+    phone: '+1 752 777 66 55',
+    email: 'gavin@hooli.com'
+  },
+  {
+    id: 3,
+    name: 'Richard Hendricks',
+    company: 'Pied Piper',
+    position: 'CEO',
+    birthDate: '01.09.1992',
+    education: 'высшее, техническое',
+    skills: 'алгоритмы сжатия, программная инженерия',
+    phone: '+1 800 123 16 14',
+    email: 'r_hendricks@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Hanneman Russ',
+    company: 'Tres Commas Distillery',
+    position: 'CEO',
+    birthDate: '31.06.1967',
+    education: 'а зачем?',
+    skills: 'радио в интернет, офигенчик',
+    phone: '+1 800 1 000 000',
+    email: 'hanneman@mail.ru'
+  },
+  {
+    id: 5,
+    name: 'Jian-Yang',
+    company: 'Erlich’s Hacker Hostel',
+    position: 'Wild-Card App Developer',
+    birthDate: '17.02.1997',
+    education: 'незаконченное высшее',
+    skills: 'китайские рецепты приготовления осьминогов',
+    phone: '+86 123 782 13 98',
+    email: 'jianyang@mail.cn'
+  },
+  {
+    id: 6,
+    name: 'Nelson "Big Head" Bighetti',
+    company: 'Hooli XYZ',
+    position: 'Head Dreamer',
+    birthDate: '18.11.1992',
+    education: 'незаконченное высшее',
+    skills: 'стрельба из артиллерийских орудий картофелем',
+    phone: '+1 125 190 17 66',
+    email: 'bighead@hooli.com'
   }
 ]
--- script.js
+++ script.js
@@ -7,13 +7,14 @@ const target = document.querySelectorAll('.row')[0];
 function createCard(template, target, data) {
   const card = template.content.cloneNode(true);
   const cardContent = card.firstElementChild;
-  cardContent.children[0].src = data.link;
+  cardContent.children[0].src = `/images/pic${data.id}.webp`;
+  cardContent.children[0].alt = data.name;
   cardContent.children[1].children[0].innerText = data.name;
   cardContent.children[1].children[1].innerText = data.company;
   cardContent.children[1].children[2].innerText = data.position;
   cardContent.children[2].children[0].innerHTML = `<b>Дата рождения:</b> ${data.birthDate}`
   cardContent.children[2].children[1].innerHTML = `<b>Образование:</b> ${data.education}`;
-  cardContent.children[2].children[2].innerHTML = `<b>Ключевые компетенции:</b> ${data.skills}`;
+  cardContent.children[2].children[2].innerHTML = `<b>Ключевые компетенции:</b> <mark>${data.skills}</mark>`;
   cardContent.children[3].children[0].innerText = data.phone;
   cardContent.children[3].children[1].innerText = data.email;
   target.append(card)
